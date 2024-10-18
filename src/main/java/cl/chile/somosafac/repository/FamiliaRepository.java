8e98806a58eea7e064854e319dee54af45c95829
21456879787a <21456879787a@gmail.com>
2024-10-17 21:22:37 -0300

    Update FamiliaRepository.java

--- src/main/java/cl/chile/somosafac/repository/FamiliaRepository.java
+++ src/main/java/cl/chile/somosafac/repository/FamiliaRepository.java
@@ -4,8 +4,12 @@ import cl.chile.somosafac.entity.FamiliaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
 import java.util.List;
+
 public interface FamiliaRepository extends JpaRepository<FamiliaEntity, Long> {
+    List<FamiliaEntity> findByNombreFaUnoContainingIgnoreCase(String nombre);
+
     @Query("SELECT f FROM FamiliaEntity f WHERE " +
             "(:nombre IS NULL OR f.nombreFaUno LIKE %:nombre%) AND " +
             "(:ciudad IS NULL OR f.comuna LIKE %:comuna%) AND " +
